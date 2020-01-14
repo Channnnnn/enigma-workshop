c5cd3112b1ad332718a1a4136d51c8810e56d494
Chan <chanchai@mycostech.com>
2020-01-14 18:57:13 +0700

    Start vue template

---
+++ vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <BoardComponent/>
+  </div>
+</template>
+
+<script>
+import BoardComponent from './components/BoardComponent.vue'
+
+export default {
+  name: 'app',
+  components: {
+    BoardComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
